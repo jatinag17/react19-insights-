1c73da4c715ada564026022876237de1e3222918
jatinag17 <jatinagrawal128@gmail.com>
2025-02-02 16:42:13 +0530

    understanding looping in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import {NetflixSeries,Footer }from "./components/NetflixSeries";
+import NetflixSeries from "./components/NetflixSeries";
 export const App = () => {
   // return React.createElement("h1", null, "Hello, world!"); // a simple js
   return (
     <div>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <Footer/>
+     
     </div>
   );
 
--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,29 +1,30 @@
-export const NetflixSeries = () => {
-  const name = "Queen of tears";
-  const rating = 5 + 3.2;
-  const returnGenre = () => {
-    const genre = "Queen";
-    return genre;
-  };
-  const age = 5;
+ import seriesData from "../api/seriesData.json";
+ const NetflixSeries = () => {
   return (
-    <>
-      <div>
-        <img src="a.jpg" alt="" style={{ width: "40%", height: "auto" }} />
-      </div>
-      <h2>Name:{name}</h2>
-      <h3>Rating:{rating}</h3>
-      <p>
-        Summary: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
-        harum unde nulla doloribus ex qui praesentium cum! Delectus cumque
-        exercitationem, maiores eum quia nemo beatae ipsa repellat numquam
-        eveniet at.
-      </p>
-      <p>Genre:{returnGenre()}</p>
-      {/* <p>{age>=18 && "you can vote"}</p>  */}
-      {/* conditional rendering   */}
-      <button>{age >= 18 ? "Watch now" : "Not Available"}</button>
-    </>
+    <ul>
+        {
+            seriesData.map((curElem)=>{
+              return ( <li key={curElem.id}>
+                  <div>
+                    <img
+                      src={curElem.img_url}
+                      alt="curElem.name"
+                      style={{ width: "40%", height: "auto" }}
+                    />
+                  </div>
+                  <h2>Name:{curElem.name}</h2>
+                  <h3>Rating:{curElem.rating}</h3>
+                  <p>Summary:{curElem.description}</p>
+                  <p>Genre:{curElem.genre}</p>
+                  <p>Cast:{curElem.cast}</p>
+                  <a href={curElem.watch_url} target="_blank">
+                    <button>Watch now</button>
+                  </a>
+                </li>
+              );
+            })}
+        
+    </ul>
   );
 };
 
@@ -31,10 +32,4 @@ export const NetflixSeries = () => {
 //?every jsx tag needs to be closed.you can use self-closing tags for elements that don't have children, e.g., <img src="url"/>
 
 
-// export default NetflixSeries;
-
-export const Footer=()=>{
-    return(
-        <p>copyright @jatin</p>
-    );
-};
+ export default NetflixSeries;
