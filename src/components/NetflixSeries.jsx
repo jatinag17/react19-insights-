f3f6b4b037ffe07373c689724b66a39cf47feea1
jatinag17 <jatinagrawal128@gmail.com>
2025-02-03 13:43:45 +0530

    understanding destructuring concept in V19

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,11 +1,11 @@
- import seriesData from "../api/seriesData.json";
+import seriesData from "../api/seriesData.json";
 import { SeriesCard } from "./Seriescard";
- const NetflixSeries = () => {
+const NetflixSeries = () => {
   return (
     <ul>
-        { seriesData.map((curElem)=>(<SeriesCard key={curElem.id} curElem={curElem}/>
-            ))}
-        
+      {seriesData.map((curElem) => (
+        <SeriesCard key={curElem.id} data={curElem} />
+      ))}
     </ul>
   );
 };
@@ -13,5 +13,4 @@ import { SeriesCard } from "./Seriescard";
 //? Each jsx expression must have one parent element,which means if you try to return multiple elements,React will throw an error.
 //?every jsx tag needs to be closed.you can use self-closing tags for elements that don't have children, e.g., <img src="url"/>
 
-
- export default NetflixSeries;
+export default NetflixSeries;
